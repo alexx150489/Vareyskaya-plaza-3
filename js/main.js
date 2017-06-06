08be84ce7605650b21666593e9d55a2a30255189
alexx150489 <nikolskiy150489@gmail.com>
2017-06-06 10:33:45 +0400

    added location page, contacts page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 $(document).ready(function() {
 
-	// jQuery Hover Delay 1.1.0 (20130401)
-// By John Terenzio | http://terenz.io/ | MIT License
-(function(b){var e=b.fn.hover,d=function(a,d,e){return this.each(function(){var c;b(this).on("mouseenter mouseleave",function(b){var f=this,g="mouseenter"===b.type?a:d;c=c?window.clearTimeout(c):window.setTimeout(function(){c=void 0;g.call(f,b)},e)})})};b.fn.hover=function(){var a=Array.prototype.slice.call(arguments);return 3===a.length&&"number"===typeof a[2]?d.apply(this,a):2===a.length&&"number"===typeof a[1]?d.call(this,a[0],a[0],a[1]):e.apply(this,a)}})(window.jQuery);
+	// slcik slider http://kenwheeler.github.io/slick/ >>>
 
-	// add-request-btn hover
-	$(".block-header .add-request-btn").hover(function() {
-		$(".block-header .bg").slideToggle();
-		}, function() {													 
-			$(".block-header .bg").slideToggle();
-	}, 100);
-
-	$(".block-footer .add-request-btn").hover(function() {
-		$(".block-footer .bg").slideToggle();
-		}, function() {													 
-			$(".block-footer .bg").slideToggle();
-	}, 100);
+	// block-location
+	$('.block-location .slider').slick({
+		arrows: true,
+		autoplay: true,
+		pauseOnHover: true,
+		autoplaySpeed: 3000,
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: $('.block-location .slide-prev'),
+		nextArrow: $('.block-location .slide-next')
+	});
 
 });
