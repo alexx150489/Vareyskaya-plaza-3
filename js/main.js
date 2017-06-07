50502395637aedcd397ad53ad4859bd07c125e51
alexx150489 <nikolskiy150489@gmail.com>
2017-06-08 00:22:29 +0400

    added about page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+ // block-mainPage SMOOTH SCROLL
+
+	$(".scrollDown-wrapper").on("click", function(e) {
+
+	e.preventDefault();
+
+	var currentBlock = $(this).attr("href"),
+	currentBlockOffset = $(currentBlock).offset().top;
+
+	$("html, body").animate({
+		scrollTop: currentBlockOffset
+	}, 500);
+
+});
+
 	// slcik slider http://kenwheeler.github.io/slick/ >>>
 
 	// block-location
@@ -15,6 +30,19 @@ $(document).ready(function() {
 		nextArrow: $('.block-location .slide-next')
 	});
 
+	// block-about technical
+	$('.block-about .technical-gallery .slider-wrapper').slick({
+		arrows: true,
+		autoplay: true,
+		pauseOnHover: true,
+		autoplaySpeed: 3000,
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: $('.block-about .technical-gallery .slide-prev'),
+		nextArrow: $('.block-about .technical-gallery .slide-next')
+	});
+
 	// block-eventsAndActions
 	$('.block-eventsAndActions .eventsAndActions-slider').slick({
 		arrows: true,
@@ -25,8 +53,22 @@ $(document).ready(function() {
 		slidesToShow: 3,
 		slidesToScroll: 1,
 		variableWidth: true,
+		infinite: false,
 		prevArrow: $('.block-eventsAndActions .eventsAndActions-slider-prev'),
 		nextArrow: $('.block-eventsAndActions .eventsAndActions-slider-next')
+	});
+
+	// block-about organisation
+	$('.block-about .organisation-slider').slick({
+		autoplay: true,
+		pauseOnHover: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: $('.organisation-gallery .slider-prev'),
+		nextArrow: $('.organisation-gallery .slider-next')
 	});
 
 });
